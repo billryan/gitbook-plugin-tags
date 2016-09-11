054468537d97484aed5ae533a48b7cb15b9d7bd8
billryan <yuanbin2014@gmail.com>
2016-09-11 17:42:37 +0800

    extract tags metadata

---
+++ index.js
@@ -0,0 +1,45 @@
+var urls = [];
+
+module.exports = {
+  book: {
+    assets: './assets',
+    css: [
+      "plugin.css"
+    ]
+  },
+  hooks: {
+    "page:before": function (page) {
+      if (this.output.name != 'website') return page;
+      if (page.tags) {
+        // extract from YAML
+        var rawtags = page.tags;
+      } else {
+        // extract from page
+        var _tag_exist = page.content.match(/[\r\n|\r|\n]\s?tags:\s?\[?(.*?)\]?[\r\n|\r|\n]/i);
+        if (!_tag_exist) return page;
+        var rawtags = _tag_exist[1].replace(/['"]+/g, '');
+      }
+      console.log('' + rawtags);
+      var lang = this.isLanguageBook()? this.language : '';
+      if (lang) lang = lang + '/';
+      console.log('path: ' + lang + page.path);
+      if (page.path === 'tags.md') {
+        console.log('processing tags.md');
+      }
+      urls.push({
+        url: this.output.toURL(lang + page.path)
+      });
+      return page;
+    },
+
+    "page": function (page) {
+      if (page.tags) console.log('tags in YAML' + page.tags);
+      console.log(this.output.toURL(page.path));
+      return page;
+    },
+
+    "finish": function () {
+
+    }
+  }
+};
