3ae7e248e7ad2b8749c932440944f82dbcc62a9e
billryan <yuanbin2014@gmail.com>
2016-09-11 21:51:00 +0800

    working for single lang

--- index.js
+++ index.js
@@ -13,6 +13,26 @@ module.exports = {
     "page:before": function(page) {
       if (this.output.name != 'website') return page;
 
+      // get page path
+      var lang = this.isLanguageBook()? this.language : '';
+      if (lang) lang = lang + '/';
+      var page_path = this.output.toURL(lang + page.path);
+
+      if (page.path === 'tags.md') {
+        for (var key in tags_map) {
+          if (tags_map.hasOwnProperty(key)) {
+            var tag_header = ''.concat(eol, '## ', key, eol);
+            page.content = page.content.concat(tag_header);
+            tags_map[key].forEach(function(e) {
+              var tag_body = ''.concat(eol, '- ', '[', e.title, ']', '(', e.url, ')');
+              page.content = page.content.concat(tag_body);
+            })
+            page.content = page.content.concat(eol);
+          }
+        }
+      }
+
+      // extract tags from page or YAML
       var rawtags = '';
       if (page.tags) {
         // extract from YAML
@@ -27,42 +47,32 @@ module.exports = {
       rawtags = '' + rawtags;
       var tags = rawtags.split(',').map(function(e) {return e.trim();});
 
-      // get page path
-      var lang = this.isLanguageBook()? this.language : '';
-      if (lang) lang = lang + '/';
-      var page_path = this.output.toURL(lang + page.path);
-      console.log('page path: ' + page_path);
-
       // push to tags_map
+      var page_title = page.content.match(/^#\s?(.*?)[\r\n|\r|\n]/)[1];
       tags.forEach(function(e) {
-        console.log('push tag ' + e);
         if (!tags_map[e]) tags_map[e] = [];
         tags_map[e].push({
           url: page_path,
-          title: page.title
+          title: page_title
         });
       })
-      console.log(tags_map);
 
-      // generate tags markdown
+      // generate tags for markdown
       var tags_md_ = [];
       tags.forEach(function(e) {
-        tags_md_.push('[' + e + ']' + '(' + lang + 'tags/#' + slug(e) + ')');
+        tags_md_.push('[' + e + ']' + '(' + lang + 'tags.html#' + slug(e) + ')');
       })
       var tags_md = eol + 'tags: ' + tags_md_.join(', ') + eol;
-      console.log('tags_md' + tags_md);
 
-      // remove tags info from page
-      // page.content = page.content.
-      if (page.path === 'tags.md') {
-        console.log('processing tags.md');
-      }
+      // override tags in markdown page
+      page.content = page.content.replace(/[\r\n|\r|\n]\s?tags:\s?\[?(.*?)\]?[\r\n|\r|\n]/ig, '');
+      // replace tags info from page and YAML
+      page.content = page.content.replace(/^#\s?(.*?)[\r\n|\r|\n]/ig, '# ' + page_title + eol + tags_md + eol);
+
       return page;
     },
 
     "page": function(page) {
-      if (page.tags) console.log('tags in YAML' + page.tags);
-      console.log(this.output.toURL(page.path));
       return page;
     },
 
