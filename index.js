d0ec75c9ac62c1554bc8731dba8aa41fb04177b7
billryan <yuanbin2014@gmail.com>
2016-09-18 20:36:22 +0800

    refactor regex

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ module.exports = {
       } else {
         // extract from page
         page.content = page.content.concat(eol);  // prevent no end of line
-        var _tag_exist = page.content.match(/[\r\n]\s*tags:\s*\[*(.*?)\]*[\r\n]/i);
+        var _tag_exist = page.content.match(/^\s*tags:\s*\[*(.*?)\]*$/im);
         if (!_tag_exist) return page;
         rawtags = _tag_exist[1];
       }
@@ -50,13 +50,12 @@ module.exports = {
       })
 
       // push to tags_map
-      var page_title = page.content.match(/^#\s*(.*?)[\r\n]/)[1];
       var page_url = this.output.toURL(page.path);
       tags.forEach(function(e) {
         if (!tags_map[e]) tags_map[e] = [];
         tags_map[e].push({
           url: page_url,
-          title: page_title
+          title: page.title
         });
       })
 
@@ -68,15 +67,16 @@ module.exports = {
       var tags_md = eol + '<i class="fa fa-tags" aria-hidden="true"></i> ' + tags_md_.join(' ') + eol;
 
       // override tags in markdown page
-      page.content = page.content.replace(/[\r\n]\s?tags:\s?\[?(.*?)\]?[\r\n]/i, eol);
+      page.content = page.content.replace(/^\s?tags:\s?\[?(.*?)\]?$/im, eol);
       // replace tags info from page and YAML
-      var title_tags = '# '.concat(page_title, eol, '<!-- tags -->', tags_md, eol, '<!-- tagsstop -->', eol);
-      page.content = page.content.replace(/^#\s*(.*?)[\r\n]/, title_tags);
+      var tags_format = eol.concat('<!-- tags -->', tags_md, eol, '<!-- tagsstop -->', eol);
+      page.content = page.content.replace(/^#\s*(.*?)$/m, '# $1' + tags_format);
 
       return page;
     },
 
     "page": function(page) {
+      // add tags id and class
       page.content = page.content.replace('<!-- tags -->', '<!-- tags --><div id="tags" class="tags">');
       page.content = page.content.replace('<!-- tagsstop -->', '</div><!-- tagsstop -->');
       return page;
